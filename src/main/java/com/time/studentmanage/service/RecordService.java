2f857d4479676b25996abb384ab636995b71a9e7
Kim-youngjae <youngjae459@gmail.com>
2024-03-13 18:00:33 +0900

    feat: 피드백 저장/수정 로직 기능 추가, 서비스 테스트

--- src/main/java/com/time/studentmanage/service/RecordService.java
+++ src/main/java/com/time/studentmanage/service/RecordService.java
@@ -1,9 +1,17 @@
 package com.time.studentmanage.service;
 
+import com.time.studentmanage.domain.Records;
+import com.time.studentmanage.domain.dto.RecordSaveReqDTO;
+import com.time.studentmanage.domain.member.Student;
+import com.time.studentmanage.domain.member.Teacher;
 import com.time.studentmanage.repository.RecordsRepository;
+import com.time.studentmanage.repository.StudentRepository;
+import com.time.studentmanage.repository.TeacherRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -11,5 +19,44 @@ import org.springframework.transaction.annotation.Transactional;
 public class RecordService {
 
     private final RecordsRepository recordsRepository;
+    private final StudentRepository studentRepository;
+    private final TeacherRepository teacherRepository;
 
+    public Long saveFeedback(RecordSaveReqDTO recordSaveReqDTO) {
+
+        Student studentPS = null;
+        Teacher teacherPS = null;
+
+        Optional<Student> studentOP = studentRepository.findById(recordSaveReqDTO.getStudentId());
+        Optional<Teacher> teacherOP = teacherRepository.findById(recordSaveReqDTO.getTeacherId());
+
+        if (studentOP.isPresent() && teacherOP.isPresent()) {
+            studentPS = studentOP.get();
+            teacherPS = teacherOP.get();
+        }
+
+        if (studentPS == null) {
+            // 학생이 없어요
+        }
+
+        if (teacherPS == null) {
+            // 선생이 없어요
+        }
+
+        Records record = recordSaveReqDTO.toEntity(teacherPS, studentPS, recordSaveReqDTO.getContent());
+        Records result = recordsRepository.save(record);
+
+        return result.getId();
+    }
+
+    public void modifyContent(Long recordId, String content) {
+        Optional<Records> recordOP = recordsRepository.findById(recordId);
+
+        if (recordOP.isPresent()) {
+            Records recordPS = recordOP.get();
+            recordPS.changeContent(content);
+        } else {
+            // TODO: 예외 발생 시키기
+        }
+    }
 }
--- src/test/java/com/time/studentmanage/service/RecordServiceTest.java
+++ src/test/java/com/time/studentmanage/service/RecordServiceTest.java
@@ -1,37 +1,106 @@
 package com.time.studentmanage.service;
 
+import com.time.studentmanage.domain.Records;
+import com.time.studentmanage.domain.dto.RecordSaveReqDTO;
+import com.time.studentmanage.domain.member.Student;
+import com.time.studentmanage.domain.member.Teacher;
 import com.time.studentmanage.repository.RecordsRepository;
+import com.time.studentmanage.repository.StudentRepository;
+import com.time.studentmanage.repository.TeacherRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.util.ReflectionTestUtils;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
 
+import static com.time.studentmanage.TestUtil.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+@Slf4j
 @ExtendWith(MockitoExtension.class)
 class RecordServiceTest {
 
     @Mock
     RecordsRepository recordsRepository;
+    @Mock
+    StudentRepository studentRepository;
+    @Mock
+    TeacherRepository teacherRepository;
 
     @InjectMocks
     RecordService recordService;
 
     @Test
-    void 학교별_모든_피드백_조회() {
+    void 피드백_저장_서비스_로직() {
         //given
+        Long fakeId = 1L;
+        String content = "피드백 내용입니다.";
+
+        Student student = createStudent();
+        Teacher teacher = createTeacher();
+        ReflectionTestUtils.setField(student, "id", fakeId);
+        ReflectionTestUtils.setField(teacher, "id", fakeId);
+
+        // stub 1
+        when(studentRepository.findById(anyLong())).thenReturn(Optional.of(student));
+        when(teacherRepository.findById(anyLong())).thenReturn(Optional.of(teacher));
+
+        // stub 2
+        Records records = Records.builder()
+                .teacher(teacher)
+                .student(student)
+                .content(content)
+                .build();
+        ReflectionTestUtils.setField(records, "id", fakeId);
+
+        when(recordsRepository.save(any())).thenReturn(records);
+
+        // 1. 저장 요청
+        RecordSaveReqDTO recordSaveReqDTO = RecordSaveReqDTO.builder()
+                .studentId(fakeId)
+                .teacherId(fakeId)
+                .content(content)
+                .build();
 
         //when
+        Long recordId = recordService.saveFeedback(recordSaveReqDTO);
+
         //then
+        assertThat(recordId).isEqualTo(fakeId);
+        assertThat(records.getStudent().getName()).isEqualTo("철수");
+        assertThat(records.getContent()).isEqualTo(content);
+
     }
 
     @Test
-    void RecordServiceTest() {
+    void 피드백_수정_서비스_로직() {
         //given
+        Long fakeId = 1L;
+        String content = "수정된 피드백 입니다.";
 
-        //when
-        //then
+        Student student = createStudent();
+        Teacher teacher = createTeacher();
+        ReflectionTestUtils.setField(student, "id", fakeId);
+        ReflectionTestUtils.setField(teacher, "id", fakeId);
+
+        // stub
+        Records record = createRecord(teacher, student);
+        log.info("before={}", record.getContent());
+        ReflectionTestUtils.setField(record, "id", fakeId);
+
+        when(recordsRepository.findById(anyLong())).thenReturn(Optional.of(record));
+
+        recordService.modifyContent(record.getId(), content);
+        log.info("after={}", record.getContent());
+
+        assertThat(record.getContent()).isEqualTo(content);
     }
+
 }
