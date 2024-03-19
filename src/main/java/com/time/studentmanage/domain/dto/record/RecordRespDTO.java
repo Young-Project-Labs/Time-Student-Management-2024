bbe187316dde7c5baab46e436051e10c7b29b197
Kim-youngjae <youngjae459@gmail.com>
2024-03-19 15:33:05 +0900

    feat: 피드백 응답 dto 필요 필드 추가

--- src/main/java/com/time/studentmanage/domain/dto/record/RecordRespDTO.java
+++ src/main/java/com/time/studentmanage/domain/dto/record/RecordRespDTO.java
@@ -1,11 +1,11 @@
 package com.time.studentmanage.domain.dto.record;
 
+import com.time.studentmanage.domain.enums.RecordStatus;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
 @Setter
@@ -14,13 +14,30 @@ public class RecordRespDTO {
     private Long id;
     private String content;
     private String teacherName;
+    private String studentName;
+    private RecordStatus status;
 
+    private LocalDateTime createDate;
     private LocalDateTime lastModifiedDate;
 
-    public RecordRespDTO(Long id, String content, String teacherName, LocalDateTime lastModifiedDate) {
+    public RecordRespDTO(Long id, String content, String teacherName, String studentName, RecordStatus status, LocalDateTime createDate, LocalDateTime lastModifiedDate) {
         this.id = id;
         this.content = content;
         this.teacherName = teacherName;
+        this.studentName = studentName;
+        this.status = status;
+        this.createDate = createDate;
         this.lastModifiedDate = lastModifiedDate;
+    }
+
+    @Override
+    public String toString() {
+        return "RecordRespDTO{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                ", teacherName='" + teacherName + '\'' +
+                ", createDate=" + createDate +
+                ", lastModifiedDate=" + lastModifiedDate +
+                '}';
     }
 }
