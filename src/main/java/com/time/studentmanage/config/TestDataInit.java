3a9b448968bc03b6a9ba95e33ac77d9f83ee643b
Kim-youngjae <youngjae459@gmail.com>
2024-03-19 15:33:31 +0900

    feat: 초기 테스트 데이터 추가

--- src/main/java/com/time/studentmanage/config/TestDataInit.java
+++ src/main/java/com/time/studentmanage/config/TestDataInit.java
@@ -1,15 +1,18 @@
 package com.time.studentmanage.config;
 
 import com.time.studentmanage.domain.Address;
+import com.time.studentmanage.domain.Record;
 import com.time.studentmanage.domain.enums.*;
 import com.time.studentmanage.domain.member.Parent;
 import com.time.studentmanage.domain.member.Student;
 import com.time.studentmanage.domain.member.Teacher;
 import com.time.studentmanage.repository.ParentRepository;
+import com.time.studentmanage.repository.RecordRepository;
 import com.time.studentmanage.repository.StudentRepository;
 import com.time.studentmanage.repository.TeacherRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 public class TestDataInit {
@@ -17,6 +20,88 @@ public class TestDataInit {
     private final StudentRepository studentRepository;
     private final TeacherRepository teacherRepository;
     private final ParentRepository parentRepository;
+    private final RecordRepository recordRepository;
+
+    @PostConstruct
+    @Transactional
+    public void initRecord() {
+        Student student1 = Student.builder()
+                .name("도라미")
+                .userId("drm@time.com").password("1234")
+                .phoneNumber("010-1111-3311").schoolName("용호초등학교")
+                .classType(ClassType.ELEMENTARY).grade(2)
+                .memberType(MemberType.STUDENT).gender(GenderType.FEMALE)
+                .address(new Address("반림동", "현대 아파트", "102-301"))
+                .attendanceStatus(AttendanceStatus.Y)
+                .build();
+
+        Teacher teacher1 = Teacher.builder()
+                .name("고길동").userId("kkd@time.com").password("1234")
+                .email("pjj@time.com").phoneNumber("010-1212-3332")
+                .memberType(MemberType.TEACHER).position(Position.TEACHER).gender(GenderType.MALE)
+                .build();
+
+        studentRepository.save(student1);
+        teacherRepository.save(teacher1);
+
+        Record record1 = Record.builder()
+                .student(student1)
+                .teacher(teacher1)
+                .content("1번 피드백 입니다.")
+                .status(RecordStatus.PUBLISHED)
+                .build();
+
+        Record record2 = Record.builder()
+                .student(student1)
+                .teacher(teacher1)
+                .content("2번 피드백 입니다.")
+                .status(RecordStatus.PUBLISHED)
+                .build();
+
+        Record record3 = Record.builder()
+                .student(student1)
+                .teacher(teacher1)
+                .content("3번 피드백 입니다.")
+                .status(RecordStatus.PUBLISHED)
+                .build();
+
+        Record record4 = Record.builder()
+                .student(student1)
+                .teacher(teacher1)
+                .content("4번 피드백 입니다.")
+                .status(RecordStatus.PUBLISHED)
+                .build();
+
+        Record record5 = Record.builder()
+                .student(student1)
+                .teacher(teacher1)
+                .content("5번 피드백 입니다.")
+                .status(RecordStatus.PUBLISHED)
+                .build();
+
+        record1.addStudent(student1);
+        record1.addTeacher(teacher1);
+
+        record2.addStudent(student1);
+        record2.addTeacher(teacher1);
+
+        record3.addStudent(student1);
+        record3.addTeacher(teacher1);
+
+        record4.addStudent(student1);
+        record4.addTeacher(teacher1);
+
+        record5.addStudent(student1);
+        record5.addTeacher(teacher1);
+
+
+        recordRepository.save(record1);
+        recordRepository.save(record2);
+        recordRepository.save(record3);
+        recordRepository.save(record4);
+        recordRepository.save(record5);
+
+    }
 
     @PostConstruct
     public void initStudent() {
