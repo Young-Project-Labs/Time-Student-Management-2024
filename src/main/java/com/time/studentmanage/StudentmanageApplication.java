fbc4c6f72ca302059ddaf05d53f25e778ddfb801
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2024-03-21 15:32:05 +0900

    chore: 시큐리티 설정 제외

--- src/main/java/com/time/studentmanage/StudentmanageApplication.java
+++ src/main/java/com/time/studentmanage/StudentmanageApplication.java
@@ -6,11 +6,12 @@ import com.time.studentmanage.repository.StudentRepository;
 import com.time.studentmanage.repository.TeacherRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableJpaAuditing
 public class StudentmanageApplication {
 
