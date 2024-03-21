cd75d6adf039ef4958cde89a0711ec7ed5176c44
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2024-03-21 15:37:23 +0900

    feat: 로그인 기능 추가 1. LoginController, HomeController 로직 추가 2. LoginService 추가(login Service) 3. student & Teacher repository에 userIdAndPassword 메서드 추가 (아이디 비밀번호 일치여부 판별을 위해 추가) 4. LoginFormDto, SessionConst 추가 하였음. 5. head & home & loginPage html 파일 추가 및 수정

---
+++ src/main/java/com/time/studentmanage/service/LoginService.java
@@ -0,0 +1,38 @@
+package com.time.studentmanage.service;
+
+import com.time.studentmanage.domain.enums.MemberType;
+import com.time.studentmanage.domain.member.Parent;
+import com.time.studentmanage.domain.member.Student;
+import com.time.studentmanage.domain.member.Teacher;
+import com.time.studentmanage.exception.DataNotFoundException;
+import com.time.studentmanage.repository.ParentRepository;
+import com.time.studentmanage.repository.StudentRepository;
+import com.time.studentmanage.repository.TeacherRepository;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class LoginService {
+    private final StudentRepository studentRepository;
+    private final ParentRepository parentRepository;
+    private final TeacherRepository teacherRepository;
+
+
+    public Optional<?> login(String loginId, String password, MemberType memberType) {
+        if (memberType == MemberType.STUDENT) {
+            return studentRepository.findByUserIdAndPassword(loginId, password);
+        }
+
+        if (memberType == MemberType.TEACHER) {
+            return teacherRepository.findByUserIdAndPassword(loginId, password);
+        }
+
+        return Optional.empty();
+    }
+}
---
+++ src/main/java/com/time/studentmanage/web/login/LoginFormDto.java
@@ -0,0 +1,16 @@
+package com.time.studentmanage.web.login;
+
+import com.time.studentmanage.domain.enums.MemberType;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString
+public class LoginFormDto {
+    @NotBlank
+    private String loginId;
+    @NotBlank
+    private String password;
+    private MemberType memberType;
+}
