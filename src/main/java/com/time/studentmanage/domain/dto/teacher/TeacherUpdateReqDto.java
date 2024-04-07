c5e3bf1fadefcb04d442575128987f51a9ae8228
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2024-04-07 12:25:46 +0900

    feat: 선생 정보 수정 및 비밀번호 수정 Dto

--- src/main/java/com/time/studentmanage/domain/dto/teacher/TeacherUpdateReqDto.java
+++ src/main/java/com/time/studentmanage/domain/dto/teacher/TeacherUpdateReqDto.java
@@ -4,32 +4,34 @@ import com.time.studentmanage.domain.enums.GenderType;
 import com.time.studentmanage.domain.enums.MemberType;
 import com.time.studentmanage.domain.enums.Position;
 import com.time.studentmanage.domain.member.Teacher;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import lombok.Builder;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TeacherUpdateReqDto {
+    @NotNull
     private Long id;
+    @NotBlank
     private String name;
+    @NotBlank
+    @Pattern(regexp="^(010|011|016|017|018|019)-\\d{3,4}-\\d{4}$")
     private String phoneNumber;
+    @NotBlank
     private String email;
+    @NotNull
     private Position position;
+    @NotNull
     private MemberType memberType;
+    @NotNull
     private GenderType gender;
 
-    @Builder
-    public TeacherUpdateReqDto(Long id, String name, String phoneNumber, String email, Position position, MemberType memberType, GenderType gender) {
-        this.id = id;
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-        this.position = position;
-        this.memberType = memberType;
-        this.gender = gender;
-    }
     //Dto -> Student 엔티티 변환
     public Teacher toEntity() {
         Teacher teacher = Teacher.builder()
