f29824a968e6e78480b19957b7b65971fcd64d78
Kim-youngjae <youngjae459@gmail.com>
2024-04-10 21:30:53 +0900

    refactor: ControllerAdvice 이름 변경

--- src/main/java/com/time/studentmanage/exception/TeacherExControllerAdvice.java
+++ src/main/java/com/time/studentmanage/exception/TeacherExControllerAdvice.java
@@ -1,14 +1,15 @@
 package com.time.studentmanage.exception;
 
+import com.time.studentmanage.web.teacher.TeacherApiController;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 @Slf4j
-@RestControllerAdvice
-public class ExControllerAdvice {
+@RestControllerAdvice(assignableTypes = TeacherApiController.class)
+public class TeacherExControllerAdvice {
     //IllegalArgumentException(500) -> BAD_REQUEST(400)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
