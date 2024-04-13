344a5a99ee7917b73e2f4bf80c269cb2e56d5987
Kim-youngjae <youngjae459@gmail.com>
2024-04-13 15:44:34 +0900

    chore: 병합 후 컴파일 오류 제거

--- src/main/java/com/time/studentmanage/domain/dto/student/StudentUpdatePwdReqDto.java
+++ src/main/java/com/time/studentmanage/domain/dto/student/StudentUpdatePwdReqDto.java
@@ -1,13 +1,6 @@
 package com.time.studentmanage.domain.dto.student;
 
-import com.time.studentmanage.domain.Address;
-import com.time.studentmanage.domain.enums.AttendanceStatus;
-import com.time.studentmanage.domain.enums.ClassType;
-import com.time.studentmanage.domain.enums.GenderType;
-import com.time.studentmanage.domain.enums.MemberType;
-import com.time.studentmanage.domain.member.Student;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
