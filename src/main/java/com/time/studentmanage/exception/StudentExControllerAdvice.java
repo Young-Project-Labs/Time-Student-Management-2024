6b954de9f3288cbef95b6a21ca422ca89952119e
Kim-youngjae <youngjae459@gmail.com>
2024-05-03 17:24:07 +0900

    feat: 예외 처리기 추가 및 오류 페이지 추가
    검색 시 발생하는 예외처리기 구현 -> 이후 공통 예외 처리기로 통합 예정
    사용자 요청 예외가 발생했을 때 처리할 400페이지 추가

---
+++ src/main/java/com/time/studentmanage/exception/StudentExControllerAdvice.java
@@ -0,0 +1,35 @@
+package com.time.studentmanage.exception;
+
+import jakarta.servlet.annotation.HttpConstraint;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class StudentExControllerAdvice {
+
+    /**
+     * 해당 예외가 발생했을 때 controller에서 처리해야하는 예외라서 ModelAndView를 반환
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = {IllegalArgumentException.class})
+    public String handleIllegalArgumentException(IllegalArgumentException ex, Model model) {
+        model.addAttribute("errorMessage", ex.getMessage());
+        model.addAttribute("buttonName", "학생 목록가기");
+        model.addAttribute("backLink", "/student/list");
+        return "error/400";
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    public String handleMethodArgumentNotValidException(Model model) {
+        model.addAttribute("errorMessage", "잘못된 페이지 요청입니다.");
+        model.addAttribute("buttonName", "학생 목록가기");
+        model.addAttribute("backLink", "/student/list");
+        return "error/400";
+    }
+}
