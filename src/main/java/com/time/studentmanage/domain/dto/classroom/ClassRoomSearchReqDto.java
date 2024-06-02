33aa1b87a4ecf0a7d089987c33ac726ceb5be1d3
Kim-youngjae <youngjae459@gmail.com>
2024-06-02 13:09:44 +0900

    feat: 학급 관련 dto 추가
    ClassRoomRespDto: 테이블에 뿌려줄 데이터
    ClassRoomSearchReqDto: 검색 요청 dto

---
+++ src/main/java/com/time/studentmanage/domain/dto/classroom/ClassRoomSearchReqDto.java
@@ -0,0 +1,21 @@
+package com.time.studentmanage.domain.dto.classroom;
+
+import com.time.studentmanage.domain.enums.SearchType;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.domain.Pageable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClassRoomSearchReqDto {
+    private SearchType searchType;
+    private String content;
+    private Long teacherId;
+    @Min(0)
+    private int page;
+}
